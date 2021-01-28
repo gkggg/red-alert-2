56572ce7ce82fc1de55ff38c5cc7d829d07c49fd
gkggg <76514813+gkggg@users.noreply.github.com>
2021-01-28 01:33:07 -0800

    testing music
    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-UnitTypes.mono.isHidden = isHidden.true;
+//UnitTypes.mono.isHidden = isHidden.true;
 //bad
+//Events.on(ClientLoadEvent, () => {
+//Core.app.openURI("https://www.youtube.com/watch?v=fqlb0p11RcE");    
+//})
+Events.on(ClientLoadEvent, () => {
+    Core.app.post(() => {
+        Vars.ui.showConfirm("hi", "good soundtrack?", () => {
+          Core.app.openURI("https://www.youtube.com/watch?v=fqlb0p11RcE");
+        });
+    });
+})
+//;
