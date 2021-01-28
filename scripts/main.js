61f14e38b22adb739c088b5d3c65eb95d901eb79
gkggg <76514813+gkggg@users.noreply.github.com>
2021-01-28 05:03:45 -0800

    epic music system
    not the best , yet..

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,17 @@
 //Core.app.openURI("https://www.youtube.com/watch?v=fqlb0p11RcE");    
 //})
 Events.on(ClientLoadEvent, () => {
+    if (Vars.mobile)
+    {
     Core.app.post(() => {
-        Vars.ui.showConfirm("hi", "good soundtrack?", () => {
-          Core.app.openURI("https://www.youtube.com/watch?v=fqlb0p11RcE");
+        Vars.ui.showConfirm("hi", "do you want a good soundtrack?\n if yes plase enable shuffle and loop", () => {
+          Core.app.openURI("https://www.youtube.com/watch?v=ENyxseq59YQ&list=PLDC670C4F09073B27");
+          Events.run(Trigger.update, () => {
+          Vars.control.sound.stop();
+          });
+          //Core.settings.musicvol = 0;
         });
     });
+    }
 })
 //;
